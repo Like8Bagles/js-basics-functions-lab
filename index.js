2f5432ec6535f68c289aa78538b536349a69082c
Anthony <anthonykmbanks@gmail.com>
2021-03-16 05:22:54 -0400

    commit tree 074de531be109a0010ca4d572bd0ec1fa4b94351

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    return Math.abs(location - 42)
+}
+
+function distanceFromHqInFeet(location) {
+    return Math.abs(location - 42) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    return Math.abs(start - end) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = Math.abs(start - destination) * 264
+    if (distance < 400) {
+        return 0
+    }
+    else if (distance >= 400, distance <= 2000) {
+        return .02 * (distance - 400)
+    }
+    else if (distance > 2000, distance <= 2500) {
+        return 25
+    }
+    else {
+        return `cannot travel that far`
+    }
+}
